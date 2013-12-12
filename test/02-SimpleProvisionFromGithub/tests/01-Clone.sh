e67f334f031d24ff6e711459852b9ea7644d2a48
Christoph Dorn <christoph@christophdorn.com>
2013-12-11 21:32:41 -0800

    versions

--- test/02-SimpleProvisionFromGithub/tests/01-Clone.sh
+++ test/02-SimpleProvisionFromGithub/tests/01-Clone.sh
@@ -1,2 +1,2 @@
-pinf github.com/pinf/pinf/v1.0.0-pre.1?test/01-SimpleCommandLinePiping/programs/say Hello
-pinf github.com/pinf/pinf/v1.0.0-pre.1?test/02-SimpleProvisionFromGithub/programs/identify
+pinf github.com/pinf/pinf/v1.0.0-pre.3?test/01-SimpleCommandLinePiping/programs/say Hello
+pinf github.com/pinf/pinf/v1.0.0-pre.3?test/02-SimpleProvisionFromGithub/programs/identify
